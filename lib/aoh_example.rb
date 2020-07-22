b0b16df1fe8c26aa235bae564007793f2d4e9e29
Luis Garcia <l.garcia24@hotmail.com>
2020-07-22 06:16:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,10 +17,10 @@ def assembled_aoh
 end
 
 def literal_aoh
-  literal_arr = [{ name:  "Don Gately", occupation: "Live-in Staff" }
-                { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-                { name:  "Pat Monteseian", occupation: "Staff" }
-                { name:  "Kate Gompert", occupation: "None" }
+  literal_arr = [{ name:  "Don Gately", occupation: "Live-in Staff" },
+                { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+                { name:  "Pat Monteseian", occupation: "Staff" },
+                { name:  "Kate Gompert", occupation: "None" },
                 { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
